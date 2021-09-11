5b6f80e3f372de3d51b8a2aaee66c8f068400b6a
Honore250 <honoremanzi4@gmail.com>
2021-09-11 17:18:56 +0200

    slides added

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
 		<title>
 
 		</title>
+		<!--<div class="splash">
+		<h1 class="fade-in">welcome to my website</h1>
+	</div>--->
 		<div class="header" id="myHeader">
 			<script type="text/javascript">
 				window.onscroll= function()
@@ -20,11 +23,53 @@
 				  		header.classList.remove("sticky");
 				  	}
 				  }
+				//  const splash = document.querrySelector('.splash');
+			//document.addEventListener('DOMContentLoaded', (e)=>{
+				//setTimeout(()=>{
+			//		splash.classList.add('display-none');
+			//	}, 2000);
+		//	})
 				</script>
-			<marquee><h1 style="color:white" >WELCOME TO MBLESSY<img src="decor_nad.jpg" width="80px" height="40px" style="margin-left: 10px"></h1></marquee></div><br>
+			<marquee><h2 style="color:white" >WELCOME TO MBLESSY<img src="logo.jpg" width="100px" height="40px" style="margin-left: 10px"></h2></marquee></div><br>
 	</head>
 	<body class="body">
+		
 		<style type="text/css">
+		/*	.splash{
+				position: fixed;
+				top: 0;
+				left: 0;
+				width: 100%;
+				height: 100vh;
+				background-color: white;
+				z-index: 200;
+				color: black;
+				text-align: center;
+				line-height: 90vh;
+			}
+			.splash.display-none{
+				position: fixed;
+				opacity: 0;
+				top: 0;
+				left: 0;
+				width: 100%;
+				height: 100vh;
+				background-color: black;
+				z-index: -10;
+				color: white;
+				text-align: center;
+				line-height: 90vh;
+				transition: all 0.5;
+			}
+			@keyframes fadeIn{
+				to{
+					opacity: 1;
+				}
+			}
+			.fade-in{
+				opacity: 0;
+				animation: fadeIn 1s ease-in forwards;
+			}*/
 				.header{
 					background: orange;
 					padding: 10px 16px;
@@ -58,6 +103,7 @@
 					display: flex;
 					align-items: center;
 					box-shadow: 0 0 25px orange;
+
 					
 				}
 
@@ -72,6 +118,53 @@
 					font-size: 13px; 
 					font-family: algerian; 
 					opacity: 0.7
+				}
+				.row: :after{
+					 content: "";
+					 clear: both;
+					 display: table;
+				}
+				.row{
+					display: flex;
+				}
+				.image{
+					margin-right: 25px; 
+					margin-left: 25px;
+					flex: 33.33%;
+					margin-top: 75px;
+				}
+				.ibirayi{
+					display: flex;
+					margin-right: 25px; 
+					margin-left: 25px;
+					flex: 33.33%;
+					padding: 5px;
+					margin-top: 25px;
+
+				}
+				.igitoki{
+					display: flex;
+				}
+				.inyama{
+					display: flex;
+				}
+				.pork{
+					display: flex;
+				}
+				.imboga{
+					display: flex;
+				}
+				.salade{
+					display: flex;
+				}
+				.sambusa{
+					display: flex;
+				}
+				.boulette{
+					display: flex;
+				}
+				.isombe{
+					display: flex;	
 				}
 		</style>
 		
@@ -92,122 +185,143 @@
 				</ul>
 			</nav>
 		</header><br>
-		<div style="margin-right: 75px; margin-left: 75px" >
-					<img src="decor_nad.jpg" height="300px"; width="300px" style="margin-top: 75px">
+		<div class="row">
+		<div class="image" >
+					<img src="decor_nad.jpg" height="300px"; width="300px" >
 							<p style="margin-top: 0px"><b>umuceri pirawu</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				<div class="image" >
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>umuceri mix</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				<div class="image" >
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>umuceri umweru</b></p>	
+						</div>
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				<div class="ibirayi">
+				<div class="image" >
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>ibirayi byokeje</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				<div class="image" >
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>ibirayi frites</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>ibirayi saute</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				</div>
+				<div class="igitoki">
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>igitoki</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>imyumbati</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>inkoko</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				</div>
+				<div class="inyama">
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>beef</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>ihene</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>intama</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				</div>
+				<div class="pork">
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>pork</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>fish</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>epinard</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				</div>
+				<div class="imboga">
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>amashaza</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>imiteja & carotte</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>salade vert</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				</div>
+				<div class="salade">
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>salade gacumberi</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>salde pasta</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>fruit mix (inanasi, grapes, strawberry)</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				</div>
+				<div class="sambusa">
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>sambusa</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>amandazi</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>brochettes</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				</div>
+				<div class="boulette">
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>boulettes</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>saucisson</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>isombe</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
-					<img src="decor_nad.jpg" height="300px"; width="300px">
+				</div>
+				<div class="isombe" >
+				<div class="image">
+					<img src="decor_nad.jpg" height="300px"; width="300px" >
 							<p style="margin-top: 0px"><b>isombe</b></p>	
 				</div>
-				<div style="margin-right: 75px; margin-left: 75px" >
+				<div class="image">
 					<img src="decor_nad.jpg" height="300px"; width="300px">
 							<p style="margin-top: 0px"><b>imizuzu</b></p>	
 				</div>
+				</div>
+				
 				<div class="footer" style=" padding-top:2px; background-color: blue;">
         <center><b><p style="color: yellow ; padding-bottom: 5px; font-family: new time romani">MBLESSY FOOD CATERER & DECOR @2021</p></b></center>
       </div>
